a1f2f0707ef1fb7a4416a9edf466abdea7adb6d2
Amo Chohan <amo.chohan@gmail.com>
2017-08-26 23:59:10 +0100

    Use a more sensible name

--- src/complexity-analyser-config.php
+++ src/complexity-analyser-config.php
@@ -2,16 +2,17 @@
 
 return [
 
-    'php_mess_detector_path' => '/home/vagrant/Code/project/vendor/bin/',
+    'php_mess_detector_path' => '/home/vagrant/Code/drawmyattention/code-quality/vendor/bin/',
 
-    'project_root' => __DIR__,
+    'project_root' => '/home/vagrant/Code/drawmyattention/code-quality/',
 
     'scan_directories' => [
-        'app',
+        'src',
     ],
 
     'excluded_directories' => [
         'app/Support',
+        'tests',
     ],
 
     'excluded_files' => [
