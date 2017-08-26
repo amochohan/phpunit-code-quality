778b5d8b1088803181f63f9dabdeb2dc58496f44
Amo Chohan <amo.chohan@gmail.com>
2017-08-27 00:14:24 +0100

    Remove unused config

--- src/complexity-analyser-config.php
+++ src/complexity-analyser-config.php
@@ -2,10 +2,6 @@
 
 return [
 
-    'php_mess_detector_path' => '/home/vagrant/Code/drawmyattention/code-quality/vendor/bin/',
-
-    'project_root' => '/home/vagrant/Code/drawmyattention/code-quality/',
-
     'scan_directories' => [
         'src',
     ],
